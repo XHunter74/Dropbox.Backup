f1a5ca0551613a237ddb183423710e2c686bdcab
serhiy.krasovskyy <xhunter74@gmail.com>
2025-05-14 23:58:37 +0300

    Avoided foreach.

--- app/files-helper.ts
+++ app/files-helper.ts
@@ -6,21 +6,20 @@ export class FilesHelper {
 
     getFiles(folder: string, fileMasks: string[]): string[] {
         let result: string[] = [];
-        fileMasks.forEach(mask => {
+        for (let i = 0; i < fileMasks.length; i++) {
+            const mask = fileMasks[i];
             const files = fs.readdirSync(folder)
                 .filter((e: string) => e.match(mask))
                 .map(e => `${folder}${e}`);
             result = result.concat(files);
-        });
-
+        }
         return result;
     }
 
     deleteFiles(files: string[]) {
-        files = files
-            .filter(e => fs.existsSync(e));
-        files.forEach(e => {
-            fs.unlinkSync(e);
-        });
+        files = files.filter(e => fs.existsSync(e));
+        for (let i = 0; i < files.length; i++) {
+            fs.unlinkSync(files[i]);
+        }
     }
 }
